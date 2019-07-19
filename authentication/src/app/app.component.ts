631d35be30f459972095e0c286a7101f1edf70a8
Nicolas FABRE <nicolas.fabre@younited-credit.fr>
2019-07-19 16:03:26 +0200

    Add poc finance wrapper

---
+++ authentication/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None // <- this allows CSS to bleed to this component from parent app
+})
+export class AppComponent implements OnInit {
+
+  @Input() name: string;
+  @Output() helloEvt: EventEmitter<string> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ authentication/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [],
+  entryComponents: [
+    AppComponent
+  ]
+})
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap(): void {
+    const { injector } = this;
+
+    // create custom elements from angular components
+    const ngCustomElement = createCustomElement(AppComponent, { injector });
+
+    // define in browser registry
+    customElements.define('ng-el', ngCustomElement);
+  }
+}
