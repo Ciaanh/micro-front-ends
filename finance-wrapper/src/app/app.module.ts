26d3ed5c7dfd2e54ca94297040ece11b6266389e
Ciaanh <nicolas.fabre.21@outlook.fr>
2019-08-20 07:35:01 +0200

    add basic angular app

--- finance-wrapper/src/app/app.module.ts
+++ finance-wrapper/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { TopBarComponent } from "./top-bar/top-bar.component";
-import { ProductListComponent } from "./product-list/product-list.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([{ path: "", component: ProductListComponent }])
+    AppRoutingModule
   ],
-  declarations: [AppComponent, TopBarComponent, ProductListComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
-
-
+export class AppModule { }
