32d366d0ed28a0b0cfdb54dea23a7c23a2016fb2
Nicolas FABRE <nicolas.fabre@younited-credit.fr>
2019-08-23 18:19:21 +0200

    error zone.js

--- finance-wrapper/src/app/app.component.ts
+++ finance-wrapper/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'finance-wrapper';
+  title = "finance-wrapper";
+
+  helloEvent(who: string) {
+    console.log(who);
+  }
 }
